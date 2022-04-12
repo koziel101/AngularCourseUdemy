c04b245ffc70d8a1911021a2dc2f9d67b1aaf01f
Hiago Koziel <hiagokoziel100@gmail.com>
2022-04-12 07:45:03 +0200

    Creating a new component

---
+++ Module 01/first-program/src/app/server/server.component.ts
@@ -0,0 +1,20 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-server',
+    /**
+     * Other ways to declare the selector:
+     * 1) As an attribute:
+     * selector: '[app-server]'
+     * Using it on the html: <div app-server></div>
+     * 
+     * 2) As a class:
+     * selector: '.app-server'
+     * Using it on the html: <div class="app-server"></div>
+     * 
+    */
+    templateUrl: './server.component.html',
+})
+export class ServerComponent {
+    
+}
