7be28d4385ebaa11d2c6ffcde6cfc1aa8ba16e73
Hiago Koziel <hiagokoziel100@gmail.com>
2022-04-12 08:30:15 +0200

    Adding List component

--- Module 01/Project 01/recipeProject/src/app/app.module.ts
+++ Module 01/Project 01/recipeProject/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
@@ -14,8 +15,10 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
     HeaderComponent,
     RecipesComponent,
     RecipeListComponent,
+    RecipesComponent,
     RecipeDetailComponent,
-    RecipeItemComponent
+    ShoppingEditComponent,
+    ShoppingListComponent
   ],
   imports: [
     BrowserModule
